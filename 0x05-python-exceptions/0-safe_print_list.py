6e559a5033a3d8233354b7e0ca2e8a3de897d2c8
elyoguictm <gabyferal@gmail.com>
2021-05-17 17:46:48 +0000

    repair

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,6 +7,6 @@ def safe_print_list(my_list=[], x=0):
             cont += 1
         except IndexError:
             print()
-            return count
+            return cont
     print()
-    return count
+    return cont
