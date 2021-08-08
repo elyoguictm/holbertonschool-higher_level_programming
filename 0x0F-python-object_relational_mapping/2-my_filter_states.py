577f6855531f65469e9fde9204e783f403a4802f
elyoguictm <2788@holbertonschool.com>
2021-08-08 12:22:09 -0700

    fixx

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-script that takes in an argument and displays all values in
-the states table of hbtn_0e_0_usa where name matches the argument
+displays all values in the states table of hbtn_0e_0_usa
+where name matches matches an argument passed as a parameter
 """
 
 from sys import argv
