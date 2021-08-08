3d906cd71fad9889e60ea4b82f8f1ef2e7a31e51
elyoguictm <2788@holbertonschool.com>
2021-08-08 12:20:10 -0700

    add

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -17,7 +17,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host=HOST, user=USER, password=PASSWORD,
                          db=DATABASE, port=PORT)
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id".format(STATE))
+    cursor.execute("SELECT * FROM states WHERE \
+    name = '{}' ORDER BY id".format(STATE))
     querry = cursor.fetchall()
     for data in querry:
         print(data)
