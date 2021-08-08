f813f26b424471cee65c13aec928a030301b0d9c
elyoguictm <2788@holbertonschool.com>
2021-08-08 13:09:10 -0700

    add

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,10 +4,10 @@ displays all values in the states table of hbtn_0e_0_usa
 where name matches matches an argument passed as a parameter
 """
 
-if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
+import MySQLdb
+from sys import argv
 
+if __name__ == "__main__":
     HOST = "localhost"
     PORT = 3306
     USER = argv[1]
@@ -18,7 +18,7 @@ if __name__ == "__main__":
                          db=DATABASE, port=PORT)
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE \
-    name = '{}' ORDER BY id".format(STATE))
+name = '{}' ORDER BY id".format(STATE))
     querry = cursor.fetchall()
     for data in querry:
         print(data)
