691d61891081a2fa0796f1052bbc55814eb950c7
elyoguictm <2788@holbertonschool.com>
2021-08-20 15:59:08 -0700

    add

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -10,5 +10,5 @@ from sys import argv
 
 if __name__ == "__main__":
     email = {'email': argv[2]}
-    request = requests.post(argv[1], data=email)
-    print = (request.text)
+    req = requests.post(argv[1], data=email)
+    print(req.text)
