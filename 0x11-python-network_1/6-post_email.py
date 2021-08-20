0525307f8c46f3b343d7dc30ab8a1c5d58c95638
elyoguictm <2788@holbertonschool.com>
2021-08-20 15:51:52 -0700

    add

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,9 +4,9 @@ Script takes in a URL and an email address
 sends a POST request to the passed URL with the email as a parameter
 and displays the body of the response
 """
-
 import requests
 from sys import argv
+
 
 if __name__ == "__main__":
     email = {'email': argv[2]}
