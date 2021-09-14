245a58c675efe6aaaf22ba1054c16748ee07c61e
elyoguictm <2788@holbertonschool.com>
2021-09-14 15:40:55 -0700

    fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,14 +2,14 @@
 const request = require('request');
 const argv = process.argv[2];
 request(argv, function (err, resp, body) {
-    if (err) throw err;
-    let count = 0;
-    for (const film of JSON.parse(body).results) {
-	for (const character of film.characters) {
-	    if (character.includes('18')) {
-		count++;
-	    }
-	}
+  if (err) throw err;
+  let count = 0;
+  for (const film of JSON.parse(body).results) {
+    for (const character of film.characters) {
+      if (character.includes('18')) {
+        count++;
+      }
     }
-    console.log(count);
+  }
+  console.log(count);
 });
